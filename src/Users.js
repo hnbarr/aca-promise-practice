83d635575ffee644efbae8e5eb9ea84c27a0413d
Heather Barr <hnbarr@yahoo.com>
2019-05-28 20:00:05 -0500

    finished with importing connect and using mapState and mapDispatch to the users adn usersCont components

--- src/Users.js
+++ src/Users.js
@@ -1,22 +1,19 @@
 import React, { Component, Fragment } from 'react'
-import { Button } from '@material-ui/core'
+import { Button } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { setUsers } from './actions';
 
 class Users extends Component {
-    state = {
-      users: [] 
-    }
 
   getUsers = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((res)=>res.json())
     .then((data)=> {
-      this.setState({
-        users: data
-      })
+      this.props.setUsersFromApi(data)
       console.log(data)
     })
   }
-  
+
   render() {
     return (
       <Fragment>
@@ -25,11 +22,25 @@ class Users extends Component {
           <Button onClick={this.getUsers} variant="contained" color="primary">Get Users</Button>
         </div>
         <div className="users-block">
-          {/* Map through users here */}
+        {this.props.users.map((user) => console.log(user.name, user.username, user.email, user.website))}
         </div>
       </Fragment>
     )
   }
 }
 
-export default Users
+
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUsersFromApi: (users) => dispatch(setUsers(users))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Users)
